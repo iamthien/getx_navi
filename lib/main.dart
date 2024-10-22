f854e7614d46b5f89c4b50df63af06343ca8a09c
Thien Nguyen <thien.nguyen@pratek.vn>
2024-10-22 09:56:55 +0700

    navi and get.find

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_navi/count_controller.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -7,19 +8,45 @@ void main() {
   ));
 }
 
+//Screen 1
 class Home extends StatelessWidget {
-  var count = 0.obs;
-
+  final controller = Get.put(Controller());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("counter"),
+      appBar: AppBar(title: Text("counter")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GetBuilder<Controller>(
+                builder: (_) => Text(
+                      'clicks: ${controller.count}',
+                    )),
+            ElevatedButton(
+              child: Text('Next Route'),
+              onPressed: () {
+                Get.to(Second());
+              },
+            ),
+          ],
         ),
-        body: Center(
-          child: Obx(() => Text("$count")),
-        ),
-        floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.plus_one), onPressed: () => count++));
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () =>
+            controller.increment(), // Use a function reference here
+      ),
+    );
+  }
+}
+
+//Screen 2
+// Using Get.find() to retrieve the value of ctrl (Controller class -> where store count value)
+class Second extends StatelessWidget {
+  final Controller ctrl = Get.find();
+  @override
+  Widget build(context) {
+    return Scaffold(body: Center(child: Text("${ctrl.count}")));
   }
 }
